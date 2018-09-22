0ad3705bd7963be3f1f7aef10997a4b25e816897
andrew <gumenniy.oik@gmail.com>
2018-09-22 11:30:41 +0300

    init project and add basic components structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ClientsListComponent } from './clients-list/clients-list.component';
+import { ClientItemComponent } from './clients-list/client-item/client-item.component';
+import { ClientDetailComponent } from './clients-list/client-detail/client-detail.component';
+import { ClientSearchComponent } from './client-search/client-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientsListComponent,
+    ClientItemComponent,
+    ClientDetailComponent,
+    ClientSearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
